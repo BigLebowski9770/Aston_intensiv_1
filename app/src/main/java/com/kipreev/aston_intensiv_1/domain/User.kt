c8b68c3932b16ef57ade7ffdf0fa8576f7639820
Bigy <kipreevmaksim51@gmail.com>
2023-05-09 13:32:36 +0600

    Second and final commit

---
+++ app/src/main/java/com/kipreev/aston_intensiv_1/domain/User.kt
@@ -0,0 +1,7 @@
+package com.kipreev.aston_intensiv_1.domain
+
+data class User(
+    var firstName: String = "",
+    var lastName: String = "",
+    var age: Int = 0
+)
---
+++ app/src/main/java/com/kipreev/aston_intensiv_1/presentation/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.kipreev.aston_intensiv_1.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kipreev.aston_intensiv_1.R
+
+class SecondActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
